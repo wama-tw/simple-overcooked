aeec5bb05ebffeba32918189ff11de2334bc1dea
溫婕宇 <wenjieyu@wenjieyude-MacBook-Pro.local>
2021-05-28 15:58:17 +0800

    add running and border

--- levelMenuControllor.java
+++ levelMenuControllor.java
@@ -34,6 +34,7 @@ public class levelMenuControllor implements Initializable{
 	ImageView character;
 	
 	boolean goNorth = false, goSouth = false, goEast = false, goWest = false;
+	boolean running = false;
 	
     public void pressedHandle(KeyEvent e) {
         if (e.getCode() == KeyCode.UP) {
@@ -47,6 +48,9 @@ public class levelMenuControllor implements Initializable{
         }
         if (e.getCode() == KeyCode.LEFT) {
         	goWest = true;
+        }
+        if (e.getCode() == KeyCode.SHIFT) {
+        	running = true;
         }
     }
     
@@ -63,26 +67,93 @@ public class levelMenuControllor implements Initializable{
         if (e.getCode() == KeyCode.LEFT) {
         	goWest = false;
         }
+        if (e.getCode() == KeyCode.SHIFT) {
+        	running = false;
+        }
     }
     
+    // public void getMarginSize() {
+    // 	top = (field.getHeight() - (character.getFitHeight() / 2));
+    //	bottom = ((character.getFitHeight() / 2));
+    //	right = (field.getWidth() - (character.getFitWidth() / 2));
+    //	left = ((character.getFitWidth() / 2));
+    // }
+    
+    public boolean isRightMargin(double x) {
+    	if (x > (field.getWidth() - (character.getFitWidth() / 2))) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    public boolean isLeftMargin(double x) {
+    	if (x < -((character.getFitWidth() / 2))) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+
+    public boolean isTopMargin(double y) {
+    	if (y < -((character.getFitHeight() / 2))) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+
+    public boolean isBottomMargin(double y) {
+    	if (y > (field.getHeight() - (character.getFitHeight() / 2))) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    boolean foot = true;
+    Image image0 = new Image(getClass().getResource("./image/chrome_dinosaur.png").toExternalForm());
+    Image image1 = new Image(getClass().getResource("./image/chrome_dinosaur_left_foot_up.png").toExternalForm());
+	Image image2 = new Image(getClass().getResource("./image/chrome_dinosaur_right_foot_up.png").toExternalForm());
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
     	Timeline fps = new Timeline(new KeyFrame(Duration.millis(1000/60),(e)-> {
-    		if (goNorth) {
-    			character.setLayoutY(character.getLayoutY() - 1);
+    		double x = character.getLayoutX();
+    		double y = character.getLayoutY();
+    		int runSpeed = 3;
+    		if (running) {
+    			runSpeed += 3;
     		}
-    		if (goSouth) {
-    			character.setLayoutY(character.getLayoutY() + 1);
+    		if (goNorth && !isTopMargin(y)) {
+    			character.setLayoutY(y - runSpeed);
     		}
-    		if (goEast) {
-    			character.setLayoutX(character.getLayoutX() + 1);
+    		if (goSouth && !isBottomMargin(y)) {
+    			character.setLayoutY(y + runSpeed);
     		}
-    		if (goWest) {
-    			character.setLayoutX(character.getLayoutX() - 1);
+    		if (goEast && !isRightMargin(x)) {
+    			character.setLayoutX(x + runSpeed);
+    		}
+    		if (goWest && !isLeftMargin(x)) {
+    			character.setLayoutX(x - runSpeed);
     		}
 		}));
 		fps.setCycleCount(Timeline.INDEFINITE);
 		fps.play();
+		
+		Timeline walk = new Timeline(new KeyFrame(Duration.millis(1000/10),(e)-> {
+    		if (goNorth || goSouth || goEast || goWest) {
+    			if (foot) {
+    				character.setImage(image1);
+    			} else {
+    				character.setImage(image2);
+    			}
+    			foot = !foot;
+    		} else {
+				character.setImage(image0);
+    		}
+		}));
+		walk.setCycleCount(Timeline.INDEFINITE);
+		walk.play();
 	}
 
 }
